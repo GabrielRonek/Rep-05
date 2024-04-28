c4fe2a0d62fcac20dff2db5ac3de24400ca4d386
GabrielRonek <s24515@pjwstk.edu.pl>
2024-04-28 17:04:26 +0200

    rozgrzebano zadanie

---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Cw4.Repositories;
+using Cw4.DTOs;
+
+namespace Cw4.Controllers
+{
+    [Route("controller")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWarehouseRepository _warehouseRepository;
+
+        public WarehouseController(IConfiguration configuration, IWarehouseRepository warehouseRepository)
+        {
+            _configuration = configuration;
+            _warehouseRepository = warehouseRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWarehouse(WarehouseProductDTO data)
+        {
+            return Ok();
+        }
+
+        public
+    }
+}
---
+++ Repositories/WarehouseRepository.cs
@@ -0,0 +1,24 @@
+﻿using Cw4.DTOs;
+using System.Data.SqlClient;
+
+namespace Cw4.Repositories
+
+{
+    public interface IWarehouseRepository
+    {
+        
+    }
+    public class WarehouseRepository : IWarehouseRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public WarehouseRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    public interface WarehouseCheck
+        {
+
+        }
+    }
+}
